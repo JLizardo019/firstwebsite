ccae5b633d32a66ea1c665f816a8c2ac5c732804
Julie Lizardo <44945863+JLizardo019@users.noreply.github.com>
2020-02-04 14:34:00 -0500

    adding navigator elements

---
+++ main.js
@@ -0,0 +1,60 @@
+let newElement  = document.createElement("h4");
+newElement.textContent = "System Online: Course trajectory activated."
+newElement.style.zIndex = "1";
+newElement.style.textAlign = "center";
+newElement.style.color = "white";
+
+let newElement2 =  document.createElement("h4");
+newElement2.textContent = "System Offline: Off Course.";
+newElement2.style.zIndex = "1";
+newElement2.style.textAlign = "center";
+newElement2.style.color = "white";
+
+window.onload=function()
+{
+
+
+
+//     // only can be added once page is loaded
+// const newElement=document.createElement("p");
+// newElement.textContent ="Hello from Javascript!";
+// document.body.appendChild(newElement);
+
+if (navigator.onLine)
+{
+  onFunction();
+  
+}
+else
+ {
+   offFunction();
+ }
+}
+
+function onFunction() {
+
+    document.body.style.background = "url('ship.gif')";
+    document.body.style.backgroundSize  =  "cover";
+    // newElement = document.createElement("h4");
+    document.body.appendChild(newElement);
+    document.body.removeChild(newElement2);
+    // newElement.textContent = "System Online: Course trajectory activated."
+    // newElement.style.zIndex = "1";
+    // newElement.style.textAlign = "center";
+    // newElement.style.color = "white";
+    
+  
+}
+
+function offFunction()
+{
+    document.body.style.background = "url('lost.gif')";
+    document.body.style.backgroundSize  =  "cover";
+    const  newElement = document.createElement("h4");
+    document.body.appendChild(newElement2);
+    document.body.removeChild(newElement);
+    // newElement.textContent = "System Offline: Off Course.";
+    // newElement.style.zIndex = "1";
+    // newElement.style.textAlign = "center";
+    // newElement.style.color = "white";
+}
